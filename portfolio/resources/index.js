d2405efc412b7d03ae141f60624fa8c1d9c5d5df
Nathan Moyer <ne_st185@yahoo.com>
2020-11-22 23:11:21 -0800

    Began moving portfolio items into structured data

--- portfolio/resources/index.js
+++ portfolio/resources/index.js
@@ -1,16 +1,42 @@
-$( document ).ready(function(){
-    //add smooth scrolling to navbar links and footer
-    $('.navbar a, footer a[href="#myPage"]').on('click', function(event) {
-      if ( this.hash !== '' ) {
-        event.preventDefault();
-        var hash = this.hash;
-        //900 is number of milliseconds to complete animation
-        $( 'html, body' ).animate( {
-          scrollTop: $( hash ).offset().top
-        }, 900, function() {
+function getProjectData() {
+  return Promise.resolve(
+    fetch("./resources/portfolioProject.json")
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((err) => console.log(err))
+  );
+}
+
+Promise.resolve(getProjectData()).then((data) => {
+  console.log(data.projectData);
+  orderedData = data.projectData.sort(
+    (a, b) => a.projectDisplayOrder > b.projectDisplayOrder
+  );
+  reactProjects = orderedData.filter((elm) => elm.projectArea === "React");
+  d3Projects = orderedData.filter((elm) => elm.projectArea === "d3.js");
+  console.log(reactProjects);
+  console.log(d3Projects);
+});
+
+$(document).ready(function () {
+  //add smooth scrolling to navbar links and footer
+  $('.navbar a, footer a[href="#myPage"]').on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      //900 is number of milliseconds to complete animation
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        900,
+        function () {
           //add the hash to the url after animation (default behavior that was stopped earlier)
           window.location.hash = hash;
-        });
-      }
-    });
-  })
+        }
+      );
+    }
+  });
+});
