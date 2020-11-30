0de6440456b066d47274cc0e6662505dd2a9605d
Nathan Moyer <ne_st185@yahoo.com>
2020-11-30 10:25:17 -0800

    Added picture element and webp

--- portfolio/Generate-ProjectsHtml.ps1
+++ portfolio/Generate-ProjectsHtml.ps1
@@ -8,28 +8,30 @@ $portfolioItemString = @'
     <a href="#PROJURL#" target="_blank"
         ><span class="portfolio-grid-tile-link"></span
     ></a>
-    <img
-        class="portfolio-grid-thumbnail"
-        alt="#PROJTITLE# Thumbnail"
-        src="#PROJIMGURL#"
-    />
+    <picture>
+        <source
+            srcset="#PROJWEBPURL#"
+            type="image/webp"
+        />
+        <img  class="portfolio-grid-thumbnail" src="#PROJIMGURL#" alt="#PROJTITLE# Thumbnail" />
+    </picture>
     <p class="portfolio-grid-description">
         <strong>#PROJTITLE#</strong><br />#PROJDESC#
     </p>
 </div>
 '@
-$projUrl = ''; $projTitle = ''; $projImgUrl = ''; $projDescription = '';
-$portfolioProjectString = ''
 
 $portfolioData | ForEach-Object -Begin { $portfolioProjectString = ''; } -Process {
     $projUrl = $_.projectUrl; 
     $projTitle = $_.projectTitle; 
     $projImgUrl = $_.projectImageUrl; 
+    $projWebpUrl = $_.projectWebpUrl;
     $projDescription = $_.projectDescription;
 
     $portfolioProjectString += $portfolioItemString -replace '#PROJURL#', $projUrl `
         -replace '#PROJTITLE#', $projTitle `
         -replace '#PROJIMGURL#', $projImgUrl `
+        -replace '#PROJWEBPURL#', $projWebpUrl `
         -replace '#PROJDESC#', $projDescription
 } -End { Set-Content -Path ./staticPortfolioHTML.txt -Value $portfolioProjectString -Encoding UTF8 }
 
